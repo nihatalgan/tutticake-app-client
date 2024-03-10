74e91531ebf39bddc510e070137c974e1181adde
Ensha C Latheef <enshalatheef@gmail.com>
2024-03-10 13:03:18 +0100

    :busts_in_silhouette: added my account feature

---
+++ src/services/users.service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+class UsersService {
+  constructor() {
+    // Create a new instance of axios with a custom configuration
+    this.api = axios.create({
+      baseURL: import.meta.env.VITE_SERVER_URL
+      // We set our API's base URL so that all requests use the same base URL
+    });
+
+      // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use(config => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem('authToken');
+
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+
+      return config;
+    });
+  }
+
+  getUserDetails = () => {
+    return this.api.get('/users');
+  };
+
+  editUserDetails = ({ name, email, phoneNumber, address }) => {
+    return this.api.put('/users', { name, email, phoneNumber, address });
+  };
+
+}
+
+// Create one instance object
+const usersService = new UsersService();
+
+export default usersService;
