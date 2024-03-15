a080e7b549369f0c8a3e1b5638f1c85df3f14ee7
Nihat Algan <burakalgan@gmail.com>
2024-03-15 20:17:09 +0100

    add dark-light theme feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { AuthProviderWrapper } from './context/auth.context.jsx';
-import App from './App.jsx'
+import { AuthProviderWrapper } from "./context/auth.context.jsx";
+import App from "./App.jsx";
+import { ThemeProviderWrapper } from "./context/theme.context";
 
-import './index.css'
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>    
+    <Router>
       <AuthProviderWrapper>
-        <App />
-      </AuthProviderWrapper>                        
-    </Router>  
-  </React.StrictMode>,
-)
+        <ThemeProviderWrapper>
+          <App />
+        </ThemeProviderWrapper>
+      </AuthProviderWrapper>
+    </Router>
+  </React.StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
+import { useContext } from "react"; // <== ADD
+import { ThemeContext } from "./../context/theme.context"; // <== ADD
 
 function HomePage() {
-  
+  const value = useContext(ThemeContext);
   return (
-    <section className='hero-section'>
-      <h1 className="hero-title">
-        Tutti Cake
-      </h1>
+    <section className={"hero-section" + value}>
+      <h1 className="hero-title">Tutti Cake</h1>
     </section>
   );
 }
